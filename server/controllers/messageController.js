da0686ad92af758e55fd3bfd5179c7fcccdb9c6b
padmesh19 <padmesh191101@gmail.com>
2025-05-21 02:48:41 +0530

    import filename change in message controller

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,4 +1,4 @@
-import Message from "../models/message.js";
+import Message from "../models/Message.js";
 import User from "../models/User.js";
 import cloudinary from "../lib/cloudinary.js";
 import { io, userSocketMap } from "../server.js";
@@ -68,37 +68,34 @@ export const markMessageAsSeen = async (req, res) => {
   }
 };
 
-
 //Send message to selected user
 export const sendMessage = async (req, res) => {
-    try {
-        const { text, image } = req.body;
-        const receiverId = req.params.id;
-        const senderId = req.user._id;
+  try {
+    const { text, image } = req.body;
+    const receiverId = req.params.id;
+    const senderId = req.user._id;
 
-        let imageUrl;
-        if (image) {
-            const uploadResponse = await cloudinary.uploader.upload(image);
-            imageUrl = uploadResponse.secure_url
-        }
-        const newMessage = await Message.create({
-            senderId,
-            receiverId,
-            text,
-            image:imageUrl
-        })
-
-        //Emit the new message to the receiver's socket
-        const receiverSocketId = userSocketMap[receiverId];
-        if (receiverSocketId) {
-            io.to(receiverSocketId).emit("newMessage", newMessage)
-        }
-
-
-        res.json({ success: true, newMessage });
-
-    } catch (error) {
-        console.log(error.message);
-        res.json({ success: false, message: error.message });
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
     }
-}
+    const newMessage = await Message.create({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    //Emit the new message to the receiver's socket
+    const receiverSocketId = userSocketMap[receiverId];
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+
+    res.json({ success: true, newMessage });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
